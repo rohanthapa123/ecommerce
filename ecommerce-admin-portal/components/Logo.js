c809344292e5527bd771be22d4eb59d93af03d51
Rohan Thapa <thaparohan2019@gmail.com>
2023-05-17 16:06:28 +0545

    feat: login logout navbar sidebar

---
+++ ecommerce-admin-portal/components/Logo.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <Link href={"/"} className='flex gap-1'/>
+  )
+}
+
+export default Logo
--- ecommerce-admin-portal/pages/index.js
+++ ecommerce-admin-portal/pages/index.js
@@ -1,6 +1,32 @@
+import Layout from "@/components/Layout";
+import { signIn, signOut, useSession } from "next-auth/react"
+
 export default function Component() {
+  const {data:session} = useSession()
+  console.log(session);
+
+  if(session){
+    return (
+      <Layout>
+        <div className=" text-blue-900 flex justify-between p-4 text-lg font-medium w-full">
+        <p>Hello, {session.user.name} </p>
+        <div className="bg-gray-100 rounded-xl gap-1">
+          <img src={session.user.image}/>
+          <span className="px-2">{session.user.email}</span>
+        </div>
+      </div>
+        
+        <button onClick={()=> signOut()}>Sign Out</button>
+      </Layout>
+    )
+  }
   return (
-    <>Ecommerce Admin</>
+    <>
+      <div className=" text-blue-900 flex justify-center text-lg font-medium w-full">
+        <p>Sign IN</p><br/>
+      </div> 
+      <button onClick={()=> signIn('google')}>Sign In</button>
+    </>
   )
 }
 
